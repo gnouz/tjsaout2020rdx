fffee0e0aa3bc4c7cfb1d867e63a2ae388c71a03
alex <desorbaix@free.fr>
2020-08-19 00:44:40 +0200

    fin preparation store rdx

--- src/App/components/TchatSender/TchatSender.js
+++ src/App/components/TchatSender/TchatSender.js
@@ -7,7 +7,7 @@ export default class TchatSender extends Component {
   
   constructor(props){
     super(props);
-    this.state={message:'degemer mat',color:'#FFAEBE'}
+    this.state={message:'',color:'#FFFFFF'}
   }
   submitform=()=>{
     postMessage({...this.state,userId:this.props.whoiamID, date:new Date().toISOString()},e=>{this.setState({...this.state,message:''})})
@@ -16,7 +16,7 @@ export default class TchatSender extends Component {
     return (
       <div className={styles.TchatSender} style={{...this.props.style}}>
         <form name="sender" onSubmit={this.submitform}>
-          <input type="text" style={{flex:12}} value={this.state.message} onChange={e=>{this.setState({message:e.currentTarget.value})}}/>
+          <input placeholder="saisissez votre message ici" type="text" style={{flex:12}} value={this.state.message} onChange={e=>{this.setState({message:e.currentTarget.value})}}/>
           <input type="color" style={{flex:0.3}} value={this.state.color}  onChange={e=>{this.setState({color:e.currentTarget.value})}}/>
           <Mybutton color="tomato" style={{flex:2}} buttononclick={this.submitform}>Envoyer</Mybutton>
         </form>
